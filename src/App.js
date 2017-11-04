cb676e7f266bc36552b8d659b927badd92b23156
Geoffrey Chong <geoffrey.chong@domain.com.au>
2017-11-04 13:59:09 +1100

    colours and stuff?

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import Scroll from 'react-scroll';
+
 import TextQuestion from './components/text-question';
 import YesNoQuestion from './components/yes-no-question';
 import { updateField, fields } from './actions/form';
@@ -15,23 +17,33 @@ const App = () => {
     updateField(name, value);
   };
 
+  let Link = Scroll.Link;
+  let Element = Scroll.Element;
+
   return (
     <div className="App">
-      <div className="splash-page">
+      <section className="splash-page">
         <div className="splash-page__content">
           <h1>Make an informed decision<br />about your reproductive...</h1>
           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio commodi, ipsa in officia! Est natus culpa nisi inventore sit veniam ab libero harum beatae dicta nulla quis deserunt, dignissimos a!</p>
         </div>
-      </div>
+        <footer className="splash-page__footer">
+          <Link activeClass="active" to="question-age" spy={true} smooth={true} offset={50} duration={500}>
+            Begin
+          </Link>
+        </footer>
+      </section>
       <form action="">
-        <TextQuestion
-          id="age"
-          name="age"
-          label="My age is"
-          inputWidth={130}
-          value={age}
-          onChange={handleChange}
-        />
+        <Element name="question-age" className="element">
+          <TextQuestion
+            id="age"
+            name="age"
+            label="My age is"
+            inputWidth={130}
+            value={age}
+            onChange={handleChange}
+          />
+        </Element>
         <YesNoQuestion
           id="hair"
           label="Am I a bit hairier than my friends?"
